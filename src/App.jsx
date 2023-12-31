e532068675144c98aeffdf7072c8e492411b36d3
Baskar <baskar.lokesh@capgemini.com>
2023-12-31 22:33:03 +0800

    first commit 31-12-2023

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <center> <h1 > Internet Periodic Table</h1></center>
+
+<div id="PeriodicTable">
+   
+            <div id="Aa"> Aa</div>
+            <div id="Bb"> Bb</div>
+            <div id="Ff">Ff</div>
+            <div id="Gg">Gg</div>
+            <div id="Oo">Oo</div>
+            <div id="Pp">Pp</div>
+            <div id="Qq">Qq</div>
+            <div id="Xx">Xx</div>
+            <div id="Yy">Yy</div>
+         
+       
+            <div id="Hh">Hh</div>
+            <div id="Ii">Ii</div>
+            <div id="Jj">Jj</div>
+            <div id="Rr">Rr</div>
+            <div id="Ss">Ss</div>
+            <div id="Tt">Tt</div>
+            <div id="Zz">Zz</div>
+            <div id="A1">A1</div>
+            <div id="A2">A2</div>
+        
+            <div id="Cc">Cc</div>
+            <div id="Dd">Dd</div>
+            <div id="Ee">Ee</div>
+            <div id="Kk">Kk</div>
+            <div id="Ll">Ll</div>
+            <div id="Mm">Mm</div>
+            <div id="Uu">Uu</div>
+            <div id="A3">A3</div>
+            <div id="A4">A4</div>
+
+            <div id="Vv">Vv</div>
+            <div id="Ww">Ww</div>
+            <div id="Nn">Nn</div>
+            <div id="A5">A5</div>
+        
+</div>
+          </>
+  )
+}
+
+export default App
